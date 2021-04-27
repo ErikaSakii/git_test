ba9a041571ab4140324f97d4dfb677fda925afab
erika <1357754536@qq.com>
2021-04-27 19:19:02 +0800

    new file:javaFile

---
+++ gitcode/basic_Class.java
@@ -0,0 +1,13 @@
+package school;
+
+public abstract class basic_Class {
+    public String Class_id;
+    public String Class_name;
+
+
+    public basic_Class(){}
+    public basic_Class(String id,String name){
+        this.Class_id=id;
+        this.Class_name=name;
+    }
+}
